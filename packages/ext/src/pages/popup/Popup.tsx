b84dc017e4c4cdaffa6917f47809411272a40fc9
Derrick Bol <derrickbol33@gmail.com>
2023-10-21 20:44:34 +0800

    chore: add skeleton extension

---
+++ packages/ext/src/pages/popup/Popup.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Popup(): JSX.Element {
+  return (
+    <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800 text-white">
+      <h1>YiZi</h1>
+    </div>
+  );
+}
